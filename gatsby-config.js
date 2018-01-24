4b033ad610caa95a6f60fc50979bfa27a48f0a1e
Luca Colonnello <luca.colonnello91@gmail.com>
2018-01-24 23:05:32 +0000

    Add contentful plugin and env vars

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
+require('dotenv-safe').load({
+  allowEmptyValues: true
+});
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      }
+    },
+    'gatsby-plugin-react-helmet'
+  ],
 };
